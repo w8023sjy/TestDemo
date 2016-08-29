b5024e20afbfaf26f807c6351395450b4455718b
w8023sjy <sdwf_szw@163.com>
2016-08-29 20:48:26 +0800

    Signed-off-by: w8023sjy <sdwf_szw@163.com>

---
+++ MyApplication/app/src/main/java/com/szw/gittest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.szw.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int id;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
